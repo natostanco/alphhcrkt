fad195f69053fd18edc1d245cf52f45ad59e46d6
Francesco Giannelli <bassobassista@gmail.com>
2016-06-24 21:12:30 +0000

    Update phantomjs.sh

--- phantomjs/phantomjs.sh
+++ phantomjs/phantomjs.sh
@@ -7,4 +7,4 @@ apt install fontconfig freetype* -y -q
 wget -q --no-check-certificate -O ./phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2
 tar -xjf ./phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 -C ./
 rm -f ./phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2
-mv ./phantomjs-$PHANTOMJS_VERSION-linux-x86_64/ ./phantomjs
+mv ./phantomjs-$PHANTOMJS_VERSION-linux-x86_64/* ./phantomjs
