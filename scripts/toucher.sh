6b1206ae91de6a1e333924b4b954c72ade1f65dc
Francesco Giannelli <bassobassista@gmail.com>
2016-06-24 20:40:56 +0000

    [ci skip]

--- scripts/toucher.sh
+++ scripts/toucher.sh
@@ -5,4 +5,6 @@ do
   touch "$in";
 done < /host/list.txt
 
+/host/phantomjs/run.sh
+
 hhvm --mode server --config /etc/hhvm/php.ini --config /etc/hhvm/server.ini --user www-data -vPidFile=/var/run/hhvm/pid
