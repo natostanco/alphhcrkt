6f3af93e59c8a535d779292eb19a8ea0dad32a12
Francesco Giannelli <bassobassista@gmail.com>
2016-06-25 04:26:05 +0000

    Update phantomjs.sh

--- phantomjs/phantomjs.sh
+++ phantomjs/phantomjs.sh
@@ -4,7 +4,7 @@ DIR=`dirname "$(readlink -f "$0")"`
 
 sudo apt-get update && sudo apt-get install vim wget bzip2 fontconfig freetype* -y -q
 
-wget -q --no-check-certificate -O ${DIR}/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2
+wget --no-check-certificate -O ${DIR}/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2
 tar -xjf ${DIR}/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 -C ${DIR}
 rm -f ${DIR}/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2
 mv ${DIR}/phantomjs-$PHANTOMJS_VERSION-linux-x86_64/* ${DIR}/
