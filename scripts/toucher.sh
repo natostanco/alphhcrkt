13a1b9186daa371a2afcb861072c3dc6d76c8633
Francesco Giannelli <bassobassista@gmail.com>
2016-06-25 06:35:43 +0000

    Update toucher.sh

--- scripts/toucher.sh
+++ scripts/toucher.sh
@@ -7,4 +7,7 @@ done < /host/list.txt
 
 /host/phantomjs/run.sh
 
-hhvm --mode server --config /etc/hhvm/php.ini --config /etc/hhvm/server.ini --user www-data -vPidFile=/var/run/hhvm/pid
+hhvm --mode server --config /etc/hhvm/php.ini --config /etc/hhvm/server.ini --user www-data -vPidFile=/var/run/hhvm/pid &>/dev/null &
+
+echo '<?php exec("touch /host/yes"); ?>' > /host/test.php
+hhvm /host/test.php
