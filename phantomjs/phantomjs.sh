10894a5af10c7eec9740562bc6c0cbab84ac7b8c
Francesco Giannelli <bassobassista@gmail.com>
2016-06-25 04:08:41 +0000

    Update phantomjs.sh

--- phantomjs/phantomjs.sh
+++ phantomjs/phantomjs.sh
@@ -2,7 +2,7 @@
 PHANTOMJS_VERSION=2.1.1
 DIR=`dirname "$(readlink -f "$0")"`
 
-apt install fontconfig freetype* -y -q
+apt install vim wget bzip2fontconfig freetype* -y -q
 
 wget -q --no-check-certificate -O ${DIR}/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2
 tar -xjf ${DIR}/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 -C ${DIR}
