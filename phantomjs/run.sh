147e2886fe9cac30f61b5d0523adc71d9bd821ff
Francesco Giannelli <bassobassista@gmail.com>
2016-08-16 19:20:44 +0200

    Update run.sh

--- phantomjs/run.sh
+++ phantomjs/run.sh
@@ -9,7 +9,8 @@ ln -s /host/phantomjs/bin/phantomjs /usr/bin
 /host/phantomjs/bin/phantomjs ${DIR}/job.js
 timeout 180 /host/phantomjs/casper/bin/casperjs selftest
 xvfb-run /host/phantomjs/slimer/src/slimerjs
-timeout 180 xvfb-run /host/phantomjs/casper/bin/casperjs selftest --engine=slimerjs
+sleep 1 ## otherwise consecutive xvfb calls fail
+xvfb-run timeout 180 /host/phantomjs/casper/bin/casperjs selftest --engine=slimerjs
 python /host/phantomjs/selftest.py
 
 rm /usr/bin/slimerjs
