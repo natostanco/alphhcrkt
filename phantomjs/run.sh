ead1380283e5843dd5315afea957c8d057abab29
Francesco Giannelli <bassobassista@gmail.com>
2016-08-16 15:18:10 +0200

    Update run.sh

--- phantomjs/run.sh
+++ phantomjs/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR=`dirname "$(readlink -f "$0")"`
+ export SLIMERJSLAUNCHER=/usr/bin/firefox
 
 /host/phantomjs/bin/phantomjs ${DIR}/job.js
 timeout 180 /host/phantomjs/casper/bin/casperjs selftest
