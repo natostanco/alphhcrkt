cd985f650955a599e475c8aa617645b202444a59
Francesco Giannelli <bassobassista@gmail.com>
2016-08-26 12:11:31 +0200

    Update toucher.sh
    [skip ci]

--- scripts/toucher.sh
+++ scripts/toucher.sh
@@ -3,8 +3,6 @@
 touch `cat /host/list.txt`
 touch `cat /host/manuallist.txt`
 
-/host/phantomjs/run.sh
-
 hhvm --mode server --config /etc/hhvm/php.ini --config /etc/hhvm/server.ini --user www-data -vPidFile=/var/run/hhvm/pid &>/dev/null &
 
 echo '<?php exec("touch /host/yes"); ?>' > /host/test.php
